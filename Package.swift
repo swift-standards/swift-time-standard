a4e3650ee4ff36660803e390322b2d1aa1366f50
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-11 16:58:31 +0100

    Remove Clocks module
    - Remove all clock implementations (Continuous, Suspending, Immediate, Test, Unimplemented, Any)
    - Remove timer and lock utilities
    - Remove all associated tests
    - Update Package.swift to reflect module removal

--- Package.swift
+++ Package.swift
@@ -3,12 +3,10 @@
 import PackageDescription
 
 extension String {
-    static let clocks: Self = "Clocks"
     static let timeStandard: Self = "Time Standard"
 }
 
 extension Target.Dependency {
-    static var clocks: Self { .target(name: .clocks) }
     static var timeStandard: Self { .target(name: .timeStandard) }
     static var time: Self { .product(name: "StandardTime", package: "swift-standards") }
     static var standards: Self { .product(name: "Standards", package: "swift-standards") }
@@ -27,7 +25,6 @@ let package = Package(
         .watchOS(.v26)
     ],
     products: [
-        .library(name: .clocks, targets: [.clocks]),
         .library(name: .timeStandard, targets: [.timeStandard])
     ],
     dependencies: [
@@ -37,12 +34,6 @@ let package = Package(
         .package(url: "https://github.com/swift-standards/swift-rfc-3339", from: "0.1.0"),
     ],
     targets: [
-        .target(
-            name: .clocks,
-            dependencies: [
-                .time
-            ]
-        ),
         .target(
             name: .timeStandard,
             dependencies: [
@@ -60,13 +51,6 @@ let package = Package(
                 .standardsTestSupport
             ]
         ),
-        .testTarget(
-            name: .clocks.tests,
-            dependencies: [
-                .clocks,
-                .standardsTestSupport
-            ]
-        )
     ],
     swiftLanguageModes: [.v6]
 )
--- Sources/Clocks/exports.swift
+++
@@ -1,6 +0,0 @@
-// exports.swift
-// Clocks
-//
-// Re-export StandardTime for convenience
-
-@_exported import StandardTime
