ec01ed102a6e119febdfb770a3d2bed3665c17bb
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-02-23 14:39:16 +0100

    Standardize Package.swift swift settings with ecosystem/package split

--- Sources/Time Standard/Time.swift
+++ Sources/Time Standard/Time.swift
@@ -3,6 +3,6 @@
 //
 // Re-export foundation Time and provide unified access to all time standards
 
-// @_exported import StandardTime
+// @_exported import Time_Primitives
 @_exported import ISO_8601
 @_exported import RFC_5322
