e2f7b61ca364e44e53da283b74cd3da2762b7aea
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:55:09 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
@@ -20,10 +20,10 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-time-standard",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
     ],
     products: [
         .library(name: "Time Standard", targets: ["Time Standard"])
